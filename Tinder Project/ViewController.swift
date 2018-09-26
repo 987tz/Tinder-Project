a3f0c8c56e4c3313d03c76687d44f61e5a372560
Adam <francisfarago@franciss-macbook-pro.local>
2018-09-25 19:24:04 -0700

    Initial Commit

---
+++ Tinder Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder Project
+//
+//  Created by Francis Farago on 9/25/18.
+//  Copyright © 2018 Francis Farago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
