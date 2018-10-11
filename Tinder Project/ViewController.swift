d9fac3368ed6e6aa1efc74c5f1e29e9ed970612e
Adam <francisfarago@franciss-macbook-pro.local>
2018-10-10 17:51:34 -0700

    Francis Commit

--- Tinder Project/ViewController.swift
+++ Tinder Project/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Whats popping")
+        print("Adam is a reeee")
     }
 
 
